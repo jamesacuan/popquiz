447590d8e27d0cb5af12c93fcff386b993e70785
unknown <jecsacuan@NOD-03.sencorhq.net>
2015-08-28 11:56:04 +0800

    test 2

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-//echo $_SESSION['$firstrun'];
-//print_r($_SESSION);
-//echo "<script>alert(" . $_SESSION['$firstrun'] . ")</script>";
-header("location:index.php");
-?>
